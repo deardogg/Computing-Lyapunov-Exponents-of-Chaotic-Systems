b5c9382982ce38e29b39cf23ba0daac8ee70aab4
Joe <jdearing436@gmail.com>
2026-08-02 10:12:40 +0100

    Fix formatting and spacing in PPRCHAOS.m

--- PPRCHAOS.m
+++ PPRCHAOS.m
@@ -1,9 +1,9 @@
-x=rand;
-r=3.5;
-sum=0; 
+x = rand;
+r = 3.5;
+sum = 0; 
 for n = 1:1000000 %number of iterations found to be sufficient for accuracy to 4.d.p.
-    sum=sum+log(abs(r*(1-2*x))); 
-    x=r*x*(1-x); %loop structured so sum calculated for i = 0 to n-1
+    sum = sum + log(abs(r * (1 - 2 * x))); 
+    x = r * x * (1 - x); %loop structured so sum calculated for i = 0 to n-1
 end
-lambda=sum/1000000;
+lambda = sum/1000000;
 disp(lambda)
