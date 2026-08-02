abf497403569aced338d2898762687fbb6b24df4
Joe <jdearing436@gmail.com>
2026-08-02 10:15:42 +0100

    Refactor code formatting and spacing in PPRCHAOS3.m

--- PPRCHAOS3.m
+++ PPRCHAOS3.m
@@ -1,22 +1,22 @@
-xvector1=zeros(1,101);
-xvector1(1)=rand;
-r1=2;
+xvector1 = zeros(1,101);
+xvector1(1) = rand;
+r1 = 2;
 
-xvector2=zeros(1,101);
-xvector2(1)=rand;
-r2=3.8;
+xvector2 = zeros(1, 101);
+xvector2(1) = rand;
+r2 = 3.8;
 
 for i = 1:100
-    xvector1(i+1)=r1*xvector1(i)*(1-xvector1(i));
-    xvector2(i+1)=r2*xvector2(i)*(1-xvector2(i));
+    xvector1(i + 1)=r1 * xvector1(i) * (1 - xvector1(i));
+    xvector2(i + 1)=r2 * xvector2(i) * (1 - xvector2(i));
 end
 
-timeindex=linspace(0,100,101);
+timeindex = linspace(0, 100, 101);
 
 hold on
-plot(timeindex,xvector1,Color='red')
-plot(timeindex,xvector2,Color='blue')
+plot(timeindex, xvector1, Color='red')
+plot(timeindex, xvector2, Color='blue')
 xlim([0,100])
 xlabel('i = time index')
 ylabel('xi')
-legend('r=2','r=3.8')
+legend('r=2', 'r=3.8')
